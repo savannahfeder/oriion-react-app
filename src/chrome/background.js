e7fab65caab425deaa3692df6d00dd0ce1f4e85a
Savannah Feder <sfeder0@gmail.com>
2022-05-01 13:32:21 -0700

    in process of adding streaks; need to switch to chrome storage to access local storage data

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -1,12 +1,42 @@
-// Replace your background.js code here
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('This is a first install!');
+  const datesTakenCourse = new Set();
+  console.log(Window);
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  if (details.reason == "install") {
-    console.log("This is a first install!");
-  } else if (details.reason == "update") {
-    var thisVersion = chrome.runtime.getManifest().version;
-    console.log(
-      "Updated from " + details.previousVersion + " to " + thisVersion + "!"
-    );
-  }
+  chrome.alarms.create('handleStreak', { periodInMinutes: 0.1 });
 });
+
+// make alarm that triggers every 15 minutes
+chrome.alarms.onAlarm.addListener((alarm) => {
+  getCourseUrls();
+  console.log(alarm);
+});
+
+const getCourseUrls = () => {
+  userCourses = JSON.parse(localStorage.getItem('data'));
+  console.log(userCourses);
+};
+
+// if user visits course url for first time that day, increments streak by 1
+const handleStreak = () => {
+  if (!isUserTakingCourse()) return;
+  const currentDate = new Date().toLocaleDateString();
+  if (!dates.has(currentDate)) {
+    dates.add(currentDate);
+    incrementStreak();
+  }
+};
+
+const isUserTakingCourse = () => {
+  const currentURL = chrome.runtime.getURL();
+  // TODO: retreives course URL from app.js and checks if prexises match
+  const courseURL = ''; //TODO
+  return currentURL === courseURL;
+};
+
+// TODO: somehow import streak from app component (streak is in userData; but may
+// be wise to extract it out into its own piece of state), export it, and send
+// a message to app to increment streak from background script
+const incrementStreak = () => {
+  console.log('This should increment the streak!');
+};
